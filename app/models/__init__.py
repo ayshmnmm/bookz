087a4d0045d25825958139418e4ceac33ef47875
ayshmnmm <65579576+ayshmnmm@users.noreply.github.com>
2024-01-18 06:40:40 +0530

    Update database connection settings

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,7 +1,7 @@
 import psycopg2 as pg
 from config import Config
 
-db = pg.connect(Config.POSTGRES_URL)
+db = pg.connect(Config.POSTGRES_URL, keepalives=1, keepalives_idle=30, keepalives_interval=10, keepalives_count=5)
 cursor = db.cursor()
 
 with open('schema.sql', 'r') as f:
